c09123c4f4bb469fdf892530fffe2af1c91113fb
Eros-Burelli <67914735+Eros-Burelli@users.noreply.github.com>
2020-10-15 11:33:18 +0200

    Delete ImmagineService.java

--- NegozioSpring2/src/main/java/com/eros/negozio/service/ImmagineService.java
+++
@@ -1,8 +0,0 @@
-package com.eros.negozio.service;
-
-import com.eros.negozio.model.Immagine;
-
-public interface ImmagineService {
-    public void salvaImmagine(Immagine img);
-    public Immagine foundByName(String nome);
-}
